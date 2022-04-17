588a8eaa94336b1d4f2dc27f084ccef9871f14bb
karinaplemos <karinalemos@id.uff.br>
2022-04-17 01:59:39 -0300

    Inserting menu page, levels page and changing the chessboard layout.

---
+++ frontend/controller.js
@@ -0,0 +1,20 @@
+function callEasyGame(){
+    //TO DO: Chamar jogo com IA aleatória
+}
+
+function callMediumGame(){
+    //TO DO: Chamar jogo com IA min-max?
+
+}
+
+function callPlay() {
+    //TO DO: Chamar tela com opções de niveis de jogo
+}
+
+function callTutorial() {
+
+}
+
+function callAbout(){
+
+}
