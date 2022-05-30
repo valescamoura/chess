c2a873e67121886d83ec736076b6fea94b49c6c9
valescamoura <valescamoura@id.uff.br>
2022-05-30 20:31:35 -0300

    Fixing references and updating views

--- chess/urls.py
+++ chess/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('gamepage/', views.gamepage, name='gamepage'),
+    path('levels/', views.levels, name='levels'),
 ]
--- chess/views.py
+++ chess/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 def index(request): 
     return render(request, 'chess/menu.html', {})
     
+def gamepage(request):
+    return render(request, 'chess/chess.html', {})
+
+def levels(request):
+    return render(request, 'chess/levels.html', {})
