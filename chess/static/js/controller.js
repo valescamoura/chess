e70e5caea8cd0abf65993875da2f38d656cb3812
valescamoura <valescamoura@id.uff.br>
2022-05-30 21:41:20 -0300

    Fixing frontend calls

--- chess/static/js/controller.js
+++ chess/static/js/controller.js
@@ -4,17 +4,19 @@ function callEasyGame(){
 
 function callMediumGame(){
     //TO DO: Chamar jogo com IA min-max
+    
 
 }
 
 function callPlay() {
     //TO DO: Chamar tela com opções de niveis de jogo
+    alert('call play')
 }
 
 function callTutorial() {
-
+    alert('call tutorial')
 }
 
 function callAbout(){
-
+    alert('call about')
 }
