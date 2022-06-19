ec2f77dad574de3dcf6f79cb3765d857caab7a34
thiagolop1 <thiago_nascimento@id.uff.br>
2022-06-19 00:00:07 -0300

    Mudança na classe e criação de novo método para IA
    Classe de serviço agora gera um objeto do Board na sua inicialização, reiniciando o Board quando o a função start_game é chamada, além disso foi adicionada o método aleatório para IA.

--- chess/src/services.py
+++ chess/src/services.py
@@ -1,42 +1,39 @@
 from chess.src.board import *
 from chess.src.ia import *
 
-global chess
 
 class Services:
 
+    def __init__(self):
+        self.chess = Board.initial()
+
     # create the new game and return the dictionary
-    def start_game():
-        global chess
-        chess = Board.initial()
-        return chess.pieces
-    print(start_game())
+    def start_game(self):
+        self.chess = Board.initial()
+        return self.chess.pieces
 
     # receive a string and will return a list of string with the possible moves
-    def get_legal_moves(piece):
-        global chess
-        sequences = chess.getLegalSequences(piece)
+    def get_legal_moves(self, piece):
+        sequences = self.chess.getLegalSequences(piece)
         result = []
         for i in range(len(sequences)):
             sequences[i] = sequences[i][0].split(" ")
             result.append(sequences[i][2])
 
         return result
-    print(get_legal_moves("a7"))
 
     # receive a string to represent the piece and another to represent where it should go
     # output is the new dictionary
-    def execute_move(piece, final): # APAGAR: ids
-        global chess
+    def execute_move(self, piece, final):
         move = "move " + piece + " " + final
-        sequences = chess.getLegalSequences(piece)
+        sequences = self.chess.getLegalSequences(piece)
         executed = False
         newType = ""
         promotion = False
 
-        if chess.pieces[piece]["team"] == "white":
+        if self.chess.pieces[piece]["team"] == "white":
             if final[1] == "8":
-                if chess.pieces[piece]["type"] == "pawn":
+                if self.chess.pieces[piece]["type"] == "pawn":
                     newType = input()
                     promotion = True
 
@@ -45,61 +42,59 @@ class Services:
                     if promotion:
                         if newType == sequences[i][1].split()[3]:
                             for j in range(len(sequences[i])):
-                                chess.execute(sequences[i][j])
+                                self.chess.execute(sequences[i][j])
                                 executed = True
                             if executed:
                                 break
 
                     else:
                         for j in range(len(sequences[i])):
-                            chess.execute(sequences[i][j])
+                            self.chess.execute(sequences[i][j])
                             executed = True
                         if executed:
                             break
 
-        return chess.pieces
+        return self.chess.pieces
 
     # IA movement
     # output is the new dictionary
-    def IAMove():
-        sequence = alpha_beta(chess)
+    def IAMove_medio(self):
+        sequence = alpha_beta(self.chess)
         if sequence:
-            chess.execute(sequence)
-        return chess.pieces #tabuleiro
+            self.chess.execute(sequence)
+        return self.chess.pieces #tabuleiro
+
+    def IAMove_facil(self):
+        sequence = rand(self.chess)
+        if sequence:
+            self.chess.execute(sequence)
+        return self.chess.pieces #tabuleiro
 
     # receive the team and return true if there is a possible move for the team
-    def legalSequences(team):
-        global chess
+    def legalSequences(self, team):
         movements = []
-        for piece in chess.pieces:
-            if chess.pieces[piece]['team'] == team:
-                movements.append(chess.getLegalSequences(piece))
+        for piece in self.chess.pieces:
+            if self.chess.pieces[piece]['team'] == team:
+                movements.append(self.chess.getLegalSequences(piece))
 
         if movements:
             return True
         else:
             return False
 
-    def drawn():
-        global chess
-        if not Services.legalSequences("white"):
-            if not chess.isKingInCheckmate("white"):
+    def drawn(self):
+        if not Services.legalSequences(self, "white"):
+            if not self.chess.isKingInCheck("white"):
                 return True
 
-        if not Services.legalSequences("black"):
-            if not chess.isKingInCheckmate("black"):
+        if not Services.legalSequences(self, "black"):
+            if not self.chess.isKingInCheck("black"):
                 return True
 
         return False
 
-    def didPlayerWin():
-        global chess
-        return chess.isKingInCheckmate("black")
-    
-    print(didPlayerWin())
+    def didPlayerWin(self):
+        return self.chess.isKingInCheckmate("black")
 
-    def didIAWin():
-        global chess
-        return chess.isKingInCheckmate("white")
-    print(didIAWin())
-
+    def didIAWin(self):
+        return self.chess.isKingInCheckmate("white")
